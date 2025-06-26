b2d5bb9b002bbbaefde77c41ab8bca9084636257
etil2jz <81570777+etil2jz@users.noreply.github.com>
2025-06-26 22:22:02 +0200

    Modules Maven pour client et serveur

---
+++ oracolo-f1-server/src/main/java/etil2jz/oracolo/Main.java
@@ -0,0 +1,12 @@
+package etil2jz.oracolo;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+	private static final Logger logger = LogManager.getLogger(Main.class);
+
+	public static void main(String[] args) {
+		logger.info("Démarrage de Oracolo F1 - Server");
+	}
+}
